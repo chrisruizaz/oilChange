40ba8defce9c9549877d41e8a4ba63af4736847a
Christopher Ruiz <chrisruizaz@gmail.com>
2020-03-09 21:37:12 -0700

    multipass set up

---
+++ resources/javascript/app.js
@@ -0,0 +1,17 @@
+/* multipass */
+console.log(
+  `--------------------------------------------------------------------------------`
+);
+console.log(`Thank you for using multipass!`);
+console.log(`Type multipass() into the console for more information.`);
+console.log(
+  `--------------------------------------------------------------------------------`
+);
+console.log(`launching ./resources/javascript/app.js...`);
+$(document).ready(function() {
+  console.log(`app.js ready!`);
+  /* ----- Code Start ----- */
+
+  /* ------ Code End ------ */
+});
+console.log(`loading...`);
